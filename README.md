7298aad518e4351c958d4b48b8b000c7f3686cb3
hmalak <hmalak@spartaglobal.com>
2020-07-06 08:49:05 +0100

    comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Encapsulation goes here 
-[]()
+Encapsulation is the act of makming variable private, only to be edited within the class itself and not outside of it 
--- encap.py
+++ encap.py
@@ -1,11 +1,11 @@
 # car has two private variable
 class Car:
-    __maxspeed = 0
-    __name = ""
+    __maxspeed = 0 # made private
+    __name = "" #made private
 
     def __init__(self):
-        self.__maxspeed = 200
-        self.__name = "Supercar"
+        self.__maxspeed = 200 #private vairable cannot be called
+        self.__name = "Supercar" #private vairable cannot be called
 
     def drive(self):
         print("driving " + str(self.__maxspeed))
@@ -16,5 +16,6 @@ class Car:
 
 carblue = Car()
 carblue.drive()
-carblue.__maxspeed = 10  # cannot be called as we have made it private
+carblue.__maxspeed = 10  # cannot be changed as we have made it private
 carblue.drive()
+carblue._aircon()
